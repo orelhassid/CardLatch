074088461b50622ec27ef2c143974731d8c848a5
orel hassid <nowayback3d@gmail.com>
2020-07-27 12:15:41 +0300

    upload lottie player and modiy speed

--- src/animations/overnight.js
+++ src/animations/overnight.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Lottie from "react-lottie";
 import animationData from "./overnight.json";
 
 export default function Overnight() {
+  const [speed, setSpeed] = useState();
+
+  useEffect(() => {
+    setSpeed(5);
+  }, []);
+
   const defaultOptions = {
     loop: true,
     autoplay: true,
@@ -14,7 +20,7 @@ export default function Overnight() {
 
   return (
     <div className="animation">
-      <Lottie options={defaultOptions} />
+      <Lottie options={defaultOptions} speed={speed} />
     </div>
   );
 }
